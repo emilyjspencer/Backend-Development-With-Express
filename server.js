30ccd10db1c8e24aeb9855f319d9efafd84c2cce
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-08 18:21:05 +0000

    Adds Express's static function

--- server.js
+++ server.js
@@ -3,22 +3,7 @@
   var express = require('express')
   var app = express(); // calling the express() function will initialize the server
 
-   // handler function 
-   app.get("/", function (req, res) {
-     res.send("Woo Node!!!!");
-   });
-  
-  app.get("/node", function (req, res) {
-      res.send("Node is fun");
-  });
-
-  app.get("/hedgehog", function(req, res) {
-    res.send("My name is Matilda. I am a hedgehog")
-  });
-
-  app.get("/britney", function(req, res) {
-    res.send("It's Britney, b****")
-  });
+  app.use(express.static("public"));
 
   
 
