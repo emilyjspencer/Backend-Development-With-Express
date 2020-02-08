62f2fac576d67b9fe1068919c0d1ac3a1fea6dad
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-08 18:11:50 +0000

    Adds handler function

--- server.js
+++ server.js
@@ -3,9 +3,17 @@
   var express = require('express')
   var app = express(); // calling the express() function will initialize the server
 
+   // handler function 
+   app.get("/", function (req, res) {
+     res.send("Woo Node!!!!")
+
+  });
+
   app.listen(3000, function() { // set a port for the  server to listen to 
     console.log('Server is listening on port 3000. Ready to accept requests eheheheheh!');
   });
+
+ 
 
   // app.listen() takes two args - port and a callback function - telling it what to do once the server
   // is running 
