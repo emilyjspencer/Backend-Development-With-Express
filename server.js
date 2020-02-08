23420d9b87bb59cc1bfb17d6ae85f4cb5a021ac4
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-08 17:58:40 +0000

    Creates server

---
+++ server.js
@@ -0,0 +1,15 @@
+
+
+  var express = require('express')
+  var app = express(); // calling the express() function will initialize the server
+
+  app.listen(3000, function() { // set a port for the  server to listen to 
+    console.log('Server is listening on port 3000. Ready to accept requests eheheheheh!');
+  });
+
+  // app.listen() takes two args - port and a callback function - telling it what to do once the server
+  // is running 
+
+  // I will run the server on port 3000, and call console.log in the callback function
+
+  // run the server by typing: node server.js
